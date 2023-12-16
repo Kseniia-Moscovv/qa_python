fe9998312a10142fb4b25c7512868c2b34879508
Kseniia Moscovvkina <miserablelie@yandex.ru>
2023-12-16 17:07:51 +0100

    Added tests for BooksCollector app

--- tests.py
+++ tests.py
@@ -1,24 +1,158 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.books_genre) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    @pytest.mark.parametrize(
+        'incorrect_len_book_name, status',
+        [
+            ['', None],
+            ['Самое длинное название книги в мире за все время', None]
+        ]
+    )
+    def test_add_new_book_with_incorrect_parameters(self, incorrect_len_book_name, status):
+        collector = BooksCollector()
+        collector.add_new_book(incorrect_len_book_name)
+        book_from_collector = collector.books_genre.get(incorrect_len_book_name)
+        assert book_from_collector == status
+
+    def test_add_existing_book_in_dictionary(self):
+        collector = BooksCollector()
+
+        test_book_name = 'Mowgli'
+        test_genre = collector.genre[3]
+
+        collector.add_new_book(test_book_name)
+        collector.set_book_genre(test_book_name, test_genre)
+
+        collector.add_new_book(test_book_name)
+
+        assert collector.get_book_genre(test_book_name)
+
+    def test_add_genre_to_book(self):
+        collector = BooksCollector()
+
+        test_book_name = 'Dune'
+        test_genre = collector.genre[0]
+
+        collector.add_new_book(test_book_name)
+        collector.set_book_genre(test_book_name, test_genre)
+
+        actual = collector.books_genre.get(test_book_name)
+        assert test_genre == actual
+
+    def test_add_not_existing_genre_to_book(self):
+        collector = BooksCollector()
+
+        test_book_name = 'The Catcher in the Rye'
+        not_existing_genre = 'Роман'
+
+        collector.add_new_book(test_book_name)
+        collector.set_book_genre(test_book_name, not_existing_genre)
+
+        actual = collector.books_genre.get(test_book_name)
+        assert actual == ''
+
+    def test_get_genre_by_book_name(self):
+        collector = BooksCollector()
+
+        test_book_name = 'Shining'
+        test_genre = collector.genre[1]
+
+        collector.add_new_book(test_book_name)
+        collector.set_book_genre(test_book_name, test_genre)
+
+        actual = collector.get_book_genre(test_book_name)
+        assert test_genre == actual
+
+    def test_get_specific_genre_for_three_books(self):
+        collector = BooksCollector()
+
+        horror_genre = collector.genre[1]
+        horror_book_list = [
+            'Pet Sematary',
+            'Call of Cthulhu',
+            'Dracula',
+        ]
+
+        for book in horror_book_list:
+            collector.add_new_book(book)
+            collector.set_book_genre(book, horror_genre)
+
+        actual = collector.get_books_with_specific_genre(horror_genre)
+        assert horror_book_list == actual
+
+    def test_get_all_genres_from_dictionary(self):
+        collector = BooksCollector()
+
+        strugatsky_books = {
+            'Roadside Picnic': 'Фантастика',
+            'Hard to be a god': 'Фантастика'
+        }
+        for (book, genre) in strugatsky_books.items():
+            collector.add_new_book(book)
+            collector.set_book_genre(book, genre)
+
+        actual = collector.get_books_genre()
+        assert strugatsky_books == actual
+
+    def test_book_list_for_children(self):
+        collector = BooksCollector()
+
+        different_books = {
+            'Little Prince': 'Мультфильмы',
+            'Silence of the Lambs': 'Детектив'
+        }
+
+        for (book, genre) in different_books.items():
+            collector.add_new_book(book)
+            collector.set_book_genre(book, genre)
+
+        actual = collector.get_books_for_children()
+
+        for book in actual:
+            genre = different_books.get(book)
+            assert genre not in collector.genre_age_rating
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+
+        favorite_book = 'Holy Book'
+        collector.add_new_book(favorite_book)
+        collector.add_book_in_favorites(favorite_book)
+
+        assert favorite_book in collector.favorites
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+
+        least_favorite_book = 'Cooking Book'
+        collector.add_new_book(least_favorite_book)
+        collector.add_book_in_favorites(least_favorite_book)
+        collector.delete_book_from_favorites(least_favorite_book)
+
+        assert least_favorite_book not in collector.favorites
+
+    def test_get_favorite_book_list(self):
+        collector = BooksCollector()
+
+        favorite_book = [
+            'Lolita',
+            'A Young Doctor_s Notebook'
+        ]
+        for book in favorite_book:
+            collector.add_new_book(book)
+            collector.add_book_in_favorites(book)
+
+        actual = collector.get_list_of_favorites_books()
+        assert favorite_book == actual
